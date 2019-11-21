cb32e17737caaf25912f052763a5effc055df241
taeyoung <xoxodudwkd@gmail.com>
2019-11-21 15:15:14 +0900

    git initalize

---
+++ src/Components/App/AppContainer.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import AppPresenter from './AppPresenter';
+import Store from 'store';
+
+class AppContainer extends Component {
+  state = {
+    message: 'HELLO',
+  };
+  render() {
+    return (
+      <Store.Provider value={this.state}>
+        <AppPresenter />
+      </Store.Provider>
+    );
+  }
+}
+
+export default AppContainer;
