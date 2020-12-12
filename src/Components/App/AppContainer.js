ea863dfc41a33cc3eb7991f84fa6e4a49484b154
taeyoung <xoxodudwkd@gmail.com>
2020-12-12 10:09:43 +0900

    Add seeNotification, deleteNotification

--- src/Components/App/AppContainer.js
+++ src/Components/App/AppContainer.js
@@ -5,7 +5,27 @@ import Store from 'store';
 class AppContainer extends Component {
   constructor(props) {
     super(props);
+    this._deleteNotification = id => {
+      this.setState(currentState => {
+        console.log(currentState);
+        const newState = delete currentState.notifications[id];
+        return newState;
+      });
+    };
+    this._seeNotification = id => {
+      this.setState(currentState => {
+        return {
+          ...currentState,
+          notifications: {
+            ...currentState.notifications,
+            [id]: { ...currentState.notifications[id], seen: true },
+          },
+        };
+      });
+    };
     this.state = {
+      seeNotification: this._seeNotification,
+      deleteNotification: this._deleteNotification,
       notifications: {
         '1': {
           id: 1,
